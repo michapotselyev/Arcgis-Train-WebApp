1595161459add42598a9f2918b3ec8ec2aaa0eba
unknown <hividermen@gmail.com>
2022-06-29 22:08:13 +0300

    V.0.1.2

--- src/components/RenderMap.js
+++ src/components/RenderMap.js
@@ -27,177 +27,24 @@ const RenderMap = ({ name }) => {
                 container: "container"
             });
 
-            // const popupOverLayers = {
-            //     "title": "{NP}",
-            //     "content": [{
-            //         "type": "fields",
-            //         "fieldInfos": [
-            //             {
-            //                 "fieldName": "NP",
-            //                 "label": "Номер Поля",
-            //                 "isEditable": true,
-            //                 "tooltip": "",
-            //                 "visible": true,
-            //                 "format": null,
-            //                 "stringFieldOption": "text-box"
-            //             }
-            //         ]
-            //     }]
-            // }
-
-            const popupLayers = {
-                "title": "{category}",
-                "content": [{
-                  "type": "fields",
-                  "fieldInfos": [
-                    {
-                        "fieldName": "cadnum",
-                        "label": "Cadnum",
-                        "isEditable": true,
-                        "tooltip": "",
-                        "visible": true,
-                        "format": null,
-                        "stringFieldOption": "text-box"
-                    },
-                    {
-                        "fieldName": "purpose_code",
-                        "label": "Purpose Code",
-                        "isEditable": true,
-                        "tooltip": "",
-                        "visible": true,
-                        "format": null,
-                        "stringFieldOption": "text-box"
-                    },
-                    {
-                        "fieldName": "purpose",
-                        "label": "Purpose",
-                        "isEditable": true,
-                        "tooltip": "",
-                        "visible": true,
-                        "format": null,
-                        "stringFieldOption": "text-box"
-                    },
-                    {
-                        "fieldName": "use_",
-                        "label": "Use",
-                        "isEditable": true,
-                        "tooltip": "",
-                        "visible": true,
-                        "format": null,
-                        "stringFieldOption": "text-box"
-                    },
-                    {
-                        "fieldName": "area",
-                        "label": "Area",
-                        "isEditable": true,
-                        "tooltip": "",
-                        "visible": true,
-                        "format": null,
-                        "stringFieldOption": "text-box"
-                    },
-                    {
-                        "fieldName": "unit_area",
-                        "label": "Unit Area",
-                        "isEditable": true,
-                        "tooltip": "",
-                        "visible": true,
-                        "format": null,
-                        "stringFieldOption": "text-box"
-                    },
-                    {
-                        "fieldName": "ownershipcode",
-                        "label": "Ownershipcode",
-                        "isEditable": true,
-                        "tooltip": "",
-                        "visible": true,
-                        "format": null,
-                        "stringFieldOption": "text-box"
-                    },
-                    {
-                        "fieldName": "ownership",
-                        "label": "Ownership",
-                        "isEditable": true,
-                        "tooltip": "",
-                        "visible": true,
-                        "format": null,
-                        "stringFieldOption": "text-box"
-                    },
-                    {
-                        "fieldName": "id",
-                        "label": "Id",
-                        "isEditable": true,
-                        "tooltip": "",
-                        "visible": true,
-                        "format": null,
-                        "stringFieldOption": "text-box"
-                    },
-                    {
-                        "fieldName": "address",
-                        "label": "Address",
-                        "isEditable": true,
-                        "tooltip": "",
-                        "visible": true,
-                        "format": null,
-                        "stringFieldOption": "text-box"
-                    },
-                    {
-                        "fieldName": "ObjectId",
-                        "label": "Object Id",
-                        "isEditable": true,
-                        "tooltip": "",
-                        "visible": true,
-                        "format": null,
-                        "stringFieldOption": "text-box"
-                    },
-                    {
-                        "fieldName": "Shape__Area",
-                        "label": "Shape Area",
-                        "isEditable": true,
-                        "tooltip": "",
-                        "visible": true,
-                        "format": null,
-                        "stringFieldOption": "text-box"
-                    },
-                    {
-                        "fieldName": "Shape__Length",
-                        "label": "Shape Length",
-                        "isEditable": true,
-                        "tooltip": "",
-                        "visible": true,
-                        "format": null,
-                        "stringFieldOption": "text-box"
-                    }
-                  ]
-                }]
-            }
-
-            const myLayer = new FeatureLayer({
-                url: "https://services7.arcgis.com/J3hAXnMntfOSlR8o/ArcGIS/rest/services/export/FeatureServer/0",               
-                popupTemplate: popupLayers
-            });
-            map.add(myLayer);
-
-            const url = "https://services7.arcgis.com/J3hAXnMntfOSlR8o/ArcGIS/rest/services/export/FeatureServer/0";
+            let Layer = new FeatureLayer();
+            const url = "https://mgagro.gismg.com:6443/arcgis/rest/services/PORTAL_30/MS_MAK_3/MapServer/";
+            Layer.url = url;
+            map.add(Layer);
             CreateLayer(url);
            
-            const secLayer = new FeatureLayer({
-                url: "https://services7.arcgis.com/J3hAXnMntfOSlR8o/ArcGIS/rest/services/secondexport/FeatureServer/0",
-                popupTemplate: popupLayers
-            });
-            map.add(secLayer);
-
-            if(name === "Show Layer 1") {
-                myLayer.when(function(){
-                    view.extent = myLayer.fullExtent;
-                });
-            }
-            if(name === "Show Layer 2") {
-                secLayer.when(function(){
-                    view.extent = secLayer.fullExtent;
-                });
-            }
-            view.center = [28.841, 49.98];
-            view.zoom = 8;
+            // if(name === "Show Layer 1") {
+            //     myLayer.when(function(){
+            //         view.extent = myLayer.fullExtent;
+            //     });
+            // }
+            // if(name === "Show Layer 2") {
+            //     secLayer.when(function(){
+            //         view.extent = secLayer.fullExtent;
+            //     });
+            // }
+            // view.center = [28.841, 49.98];
+            // view.zoom = 8;
 
         }).catch((err) => console.error(err));
     });
--- src/modules/autoCreateLayer.js
+++ src/modules/autoCreateLayer.js
@@ -1,18 +1,57 @@
+const popUp = require('./pop-up-template.json');
+const FieldsInfo = require('./pop-up-fields-info.json');
+const request = new XMLHttpRequest();
+var data = {};
+
 export const CreateLayer = (url) => {
-    url = String(url);
-    const array = url.split();
-    array.forEach(element => {
-        console.log(element);
-        console.log("\n");
-    });
+    const layerArr = [];
+
+    const array = url.split("/");
+    if (isNaN(array[array.length - 1])) url = url + "/";
+
     const jsonData = url + "?f=pjson";
-    const request = new XMLHttpRequest()
     request.open('GET', jsonData);
     request.responseType = 'json';
     request.send();
     request.onload = function() {
-        var data = request.response;
-        console.log(data);
-    }   
+        data = request.response;
+        if (data.error) {
+            return (alert("Error " + data.error.code + ": " + data.error.message));
+        }
+
+        if (isNaN(array[array.length - 1]) || array[array.length - 1] === "") {
+            const layers = data.layers;
+            for (let i = 0; i !== layers.length; i++) {
+                layerArr.push(url + layers[i].id + "?f=pjson");
+                // console.log(layerArr[i]);
+            }
+        }
+
+        data = {};
+
+        const jsonData1 = layerArr[0];
+        request.open('GET', jsonData1);
+        request.responseType = 'json';
+        request.send();
+        request.onload = function() {
+            data = request.response;
+            console.log(data.fields);
+        }
+        
+    }
+    // console.log(layerArr);
 }
+
+
+// if (element.subLayerIds) {
+//     element.subLayerIds.forEach(child => {
+//         // TO DO child layers render + pop up for them //
+//     });
+// }
+
+// const myLayer = new FeatureLayer({
+//     url: "https://services7.arcgis.com/J3hAXnMntfOSlR8o/ArcGIS/rest/services/export/FeatureServer/0",               
+//     popupTemplate: popupLayers
+// });
+
 
